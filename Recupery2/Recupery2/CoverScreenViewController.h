cfabd403ed69043ffa15272b0a1df74f99058ebd
Lauren Frazier <lfrazier11@gmail.com>
2016-02-27 16:30:14 -0800

    Added Cover Screen

---
+++ Recupery2/Recupery2/CoverScreenViewController.h
@@ -0,0 +1,13 @@
+//
+//  CoverScreenViewController.h
+//  Recupery2
+//
+//  Created by Lauren on 2/27/16.
+//  Copyright © 2016 Recupery. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CoverScreenViewController : UIViewController
+
+@end
---
+++ Recupery2/Recupery2/CoverScreenViewController.m
@@ -0,0 +1,37 @@
+//
+//  CoverScreenViewController.m
+//  Recupery2
+//
+//  Created by Lauren on 2/27/16.
+//  Copyright © 2016 Recupery. All rights reserved.
+//
+
+#import "CoverScreenViewController.h"
+
+@interface CoverScreenViewController ()
+
+@end
+
+@implementation CoverScreenViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
